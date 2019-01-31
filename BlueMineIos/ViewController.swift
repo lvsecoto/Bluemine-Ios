c65c25b55a8493582cb99871d28a8c3c328750d2
yuanjunyao <jbangit.com>
2019-01-31 22:08:30 +0800

    Initial Commit

---
+++ BlueMineIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueMineIos
+//
+//  Created by 袁俊耀 on 2019/1/31.
+//  Copyright © 2019 袁俊耀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
