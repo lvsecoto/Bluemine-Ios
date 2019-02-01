66c2585904ec2bf4ec359895a657e6bfb140fc72
yuanjunyao <jbangit.com>
2019-02-01 17:59:13 +0800

    加入主页面和导航

---
+++ BlueMineIos/controller/NavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  BlueMineIos
+//
+//  Created by 袁俊耀 on 2019/2/1.
+//  Copyright © 2019 袁俊耀. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavigationController: UINavigationController {
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.white
+        navigationBar.layer.shadowOpacity = 1
+    }
+}
