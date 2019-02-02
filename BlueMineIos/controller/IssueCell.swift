060e4da17d8ca76a21b2f184bcfd69e11a14c064
yuanjunyao <jbangit.com>
2019-02-02 11:53:17 +0800

    显示Issue条目

---
+++ BlueMineIos/controller/IssueCell.swift
@@ -0,0 +1,24 @@
+//
+//  IssueCell.swift
+//  BlueMineIos
+//
+//  Created by 袁俊耀 on 2019/2/2.
+//  Copyright © 2019 袁俊耀. All rights reserved.
+//
+
+import UIKit
+
+class IssueCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BlueMineIos/controller/IssueListControl.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  BlueMineIos
+//
+//  Created by 袁俊耀 on 2019/1/31.
+//  Copyright © 2019 袁俊耀. All rights reserved.
+//
+
+import UIKit
+
+class IssueListController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var issues: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        issues.delegate = self
+        issues.dataSource = self
+        issues.register(UINib(nibName: "IssueCell", bundle: nil), forCellReuseIdentifier: "issueCell")
+    }
+    
+    @IBAction func showProjects(_ sender: Any) {
+        
+    }
+    
+    @IBAction func showSettings(_ sender: Any) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "issueCell", for: indexPath)
+        
+        return cell
+    }
+
+}
+
